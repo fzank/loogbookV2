b056c130bea39ce5c1cb019241e902fa753cc26c
fzank <fernandozank@gmail.com>
2026-08-20 17:21:44 -0300

    variable adjusts related to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { auth, googleProvider } from './firebase';
 import { signInWithPopup, onAuthStateChanged, type User } from 'firebase/auth';
 import LogbookApp from './LogbookApp';
