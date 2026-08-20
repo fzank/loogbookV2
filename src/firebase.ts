29290809a4fbc782e4e5ec075d2dba220a8111ae
fzank <fernandozank@gmail.com>
2026-08-20 02:05:07 -0300

    firebase adjusts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,20 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Suas novas chaves do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCvolSsSxwZRLKUOTaB1w6o9jMdR-m3uL0",
-  authDomain: "logbook-de-tiro.firebaseapp.com", 
+  authDomain: "logbook-de-tiro.web.app", // <-- ALTERADO AQUI (Sai firebaseapp.com, entra web.app)
   projectId: "logbook-de-tiro",
   storageBucket: "logbook-de-tiro.firebasestorage.app",
   messagingSenderId: "1042557557631",
   appId: "1:1042557557631:web:ca5eb8b0ff4baafdd5584a"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exporta as ferramentas para o App e LogbookApp conseguirem usar (Isso estava faltando!)
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
