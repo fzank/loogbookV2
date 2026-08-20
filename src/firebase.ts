11521ab28e52f0bf784ce83e8b4686e13a54dd50
fzank <fernandozank@gmail.com>
2026-08-20 02:00:00 -0300

    firebase adjusts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCvolSsSxwZRLKUOTaB1w6o9jMdR-m3uL0",
-  authDomain: "logbook-de-tiro.web.app",
+  authDomain: "logbook-de-tiro.firebaseapp.com",
   projectId: "logbook-de-tiro",
   storageBucket: "logbook-de-tiro.firebasestorage.app",
   messagingSenderId: "1042557557631",
-  appId: "1:1042557557631:web:f120812c7bd0b77bd5584a"
+  appId: "1:1042557557631:web:ca5eb8b0ff4baafdd5584a"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
